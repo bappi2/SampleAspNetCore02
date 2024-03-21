80b9c60d39bbf5dc680a65dd1b0fa7af1ff60603
mmeftauddin <mmeftauddin@me.com>
2024-03-20 23:23:35 -0700

    add code

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -4,6 +4,7 @@ namespace SampleAspNetCore02.Controllers;
 
 [ApiController]
 //[Route("api/[controller]")]
+[Route("[controller]/[action]")]
 public class ValuesController : ControllerBase
 {
     // GET
@@ -14,34 +15,36 @@ public class ValuesController : ControllerBase
     //     return View();
     // }
 
-    [Route("api/get-all")]
-    [Route("get-all")]
-    [Route("getall")]
+    // [Route("api/get-all")]
+    // [Route("get-all")]
+    // [Route("getall")]
+    //[Route("[controller]/[action]")]
     public string GetAll()
     {
         return "Hello from GetAll";
     }
     
-    [Route("api/get-all-authors")]
-    [Route("getall")]
+    // [Route("api/get-all-authors")]
+    //[Route("getall")]
+    //[Route("[controller]/[action]")]
     public string GetAllAuthors()
     {
         return "Hello from GetAllAuthors";
     }
     
-    [Route("books/{id}")]
+    // [Route("books/{id}")]
     public string GetById(int id)
     {
         return "Hello from Get by Id: " + id;
     }
     
-    [Route("books/{id}/author/{authorId}")]
+    // [Route("books/{id}/author/{authorId}")]
     public string GetAuthorAddressById(int id, int authorId)
     {
         return "Hello from Get by Id: " + id + " authorId: " + authorId;
     }
     
-    [Route("search")]
+    // [Route("search")]
     public string SearchBox(int id, int authorId, string name, int rating, int price)
     {
         return "Hello from searchBox: " + id ;
